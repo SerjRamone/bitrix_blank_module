77a90dc5df8451485edf5f027021b9a848ce1f71
SerjRamone <sergey.greznov@gmial.com>
2013-02-11 17:01:04 +0400

    spaces->tab

--- blank/options.php
+++ blank/options.php
@@ -31,21 +31,21 @@ if( $REQUEST_METHOD == 'POST' && $_POST['Update'] == 'Y' ) {
      */
 
 	COption::SetOptionInt( $sModuleId, 'ACTIVE',($_POST['active'])?1:0);
-    COption::SetOptionString( $sModuleId, 'USER_PATH', trim($_POST['USER_PATH']));
+	COption::SetOptionString( $sModuleId, 'USER_PATH', trim($_POST['USER_PATH']));
 
-    //COption::SetOptionInt( $sModuleId, 'TIME_LIMIT', trim($_POST['time']));
+	//COption::SetOptionInt( $sModuleId, 'TIME_LIMIT', trim($_POST['time']));
 }
 
 /**
  * Описываем табы административной панели битрикса
  */
 $aTabs = array(
-    array(
-        'DIV'   => 'edit1',
-        'TAB'   => GetMessage('MAIN_TAB_SET'),
-        'ICON'  => 'fileman_settings',
-        'TITLE' => GetMessage('MAIN_TAB_TITLE_SET' )
-    )
+	array(
+		'DIV'	=> 'edit1',
+		'TAB'	=> GetMessage('MAIN_TAB_SET'),
+		'ICON'	=> 'fileman_settings',
+		'TITLE'	=> GetMessage('MAIN_TAB_TITLE_SET' )
+	)
 );
 
 /**
@@ -58,11 +58,11 @@ $oTabControl->Begin();
  * Ниже пошла форма страницы с настройками модуля
  */
 ?><form method="POST" enctype="multipart/form-data" action="<?echo $APPLICATION->GetCurPage()?>?mid=<?=htmlspecialchars($sModuleId)?>&lang=<?echo LANG?>">
-    <?=bitrix_sessid_post()?>
+	<?=bitrix_sessid_post()?>
 	<?$oTabControl->BeginNextTab();?>
 	<tr class="heading">
-        <td colspan="2"><?=GetMessage( 'AUTO_INFO' )?></td>
-    </tr>
+		<td colspan="2"><?=GetMessage( 'AUTO_INFO' )?></td>
+	</tr>
 	<tr>
 		<td><label for="active"><?echo GetMessage("DD_BM_LABEL")?></label></td>
 		<td><input type="checkbox" name="active" id="active" <?=(COption::GetOptionInt($sModuleId, "ACTIVE", "1"))?'checked':''?> value="Y">
@@ -70,9 +70,9 @@ $oTabControl->Begin();
 	</tr>
 	<?//$oTabControl->BeginNextTab();?>
 
-    <?$oTabControl->Buttons();?>
-    <input type="submit" name="Update" value="<?=GetMessage( 'DD_BM_BUTTON_SAVE' )?>" />
-    <input type="reset" name="reset" value="<?= GetMessage( 'DD_BM_BUTTON_RESET' )?>" />
-    <input type="hidden" name="Update" value="Y" />
-    <?$oTabControl->End();?>
+	<?$oTabControl->Buttons();?>
+	<input type="submit" name="Update" value="<?=GetMessage( 'DD_BM_BUTTON_SAVE' )?>" />
+	<input type="reset" name="reset" value="<?= GetMessage( 'DD_BM_BUTTON_RESET' )?>" />
+	<input type="hidden" name="Update" value="Y" />
+	<?$oTabControl->End();?>
 </form>
